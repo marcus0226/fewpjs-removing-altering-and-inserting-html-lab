e015c4cda5f23d3cb336aff86cf97c2ee208b798
marcus0226 <bmarcusv15@yahoo.com>
2021-04-04 12:18:30 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.className = "victory";
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Marcus is the champion!";
